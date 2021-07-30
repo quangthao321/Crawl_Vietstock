a9e492e467f3b8813ba0863b9e480db9d3cbea22
quangthao321 <73469475+quangthao321@users.noreply.github.com>
2021-07-30 09:19:43 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#str = 'Cô gái 26 tuổi bỏ việc lương cao ở big 4 kiểm'
+
+def ChuanHoa(str):
+    line = []
+    index = -1
+    print(len(str))
+    for i in range(index+1,len(str)):
+        if 50+index < i < 100+index:
+            if 50+index < i < 80+index:
+                if str[i] == '.':
+                    line.append(str[index+1:i+1])
+                    index = i
+            else:
+                if str[i] == ' ':
+                    line.append(str[index+1:i+1])
+                    index = i
+
+    line.append(str[index:])
+
+    for j in range(len(line)):
+        print(line[j])
+
+#ChuanHoa(str)
