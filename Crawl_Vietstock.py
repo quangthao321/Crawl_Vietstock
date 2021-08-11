0516abc12e51c60444d511e0071743a50695c008
quangthao321 <quang.nh152963@sis.hust.edu.vn>
2021-08-11 23:18:01 +0700

    add a csv file

--- Crawl_Vietstock.py
+++ Crawl_Vietstock.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.by import By
 from time import sleep
 from main import ChuanHoa
 import csv
-import pandas as pd
+#import pandas as pd
 #import string
-from selenium.webdriver.support.ui import WebDriverWait as W
-from selenium.webdriver.support import expected_conditions as E
+#from selenium.webdriver.support.ui import WebDriverWait as W
+#from selenium.webdriver.support import expected_conditions as E
 
 driver = webdriver.Chrome(executable_path='chromedriver.exe')
 driver.maximize_window ()
@@ -15,12 +15,12 @@ sleep(3)
 
 count_page = 0
 count_id = 0
-
+urls = []
 def Crawl_10_article():
     global count_page
     count_page+=1
     links = driver.find_elements_by_xpath('//h2[@class="channel-title"]/a')
-    urls = []
+
 
     #get 10 urls
     for link in links:
@@ -29,17 +29,19 @@ def Crawl_10_article():
         #print(url)
         if url not in urls:
             urls.append(url)
+    return urls
 
+def write_csv():
     with open('Vietstock.csv', 'w', encoding='utf-8', newline='') as file_output:
         headers = ['id', 'tittle', 'content']
         writer = csv.DictWriter(file_output, delimiter=',', lineterminator='\n', fieldnames=headers)
         writer.writeheader()
+        count_id = 0
         #10 urls in 1 page
         for url in urls:
-            global count_id
-            count_id += 1
+            count_id+=1
             driver.get(url)
-            sleep(4)
+            sleep(3)
             # Tìm và in tiêu đề bài báo
             tittle = driver.find_element_by_class_name('article-title')
             # list1 = ChuanHoa(tittle.text)
@@ -50,7 +52,7 @@ def Crawl_10_article():
             list_content = []
             list_tittle.append(tittle.text)
             list_content.append(content.text)
-            sleep(3)
+            sleep(2)
 
             writer.writerow({headers[0]:count_id, headers[1]:list_tittle, headers[2]:list_content})
 
@@ -59,7 +61,7 @@ def NextPage():
     next_page = driver.find_element_by_xpath('//*[@id="page-next "]/a')
     driver.execute_script("arguments[0].click();", next_page)
     sleep(3)
-    print('All right')
+    #print('All right')
 
 
 Crawl_10_article()
@@ -71,7 +73,8 @@ while True:
     for i in range(0,count_page):
         NextPage()
     Crawl_10_article()
-    if count_page == 3:
+    if count_page == 50:
         break
+    write_csv()
 
 driver.close()
