4c2ecc0765c642ca259afc917ad73075cdceab2c
quangthao321 <73469475+quangthao321@users.noreply.github.com>
2021-08-11 18:06:13 +0700

    create csv file

--- Crawl_Vietstock.py
+++ Crawl_Vietstock.py
@@ -10,6 +10,7 @@ driver = webdriver.Chrome(executable_path='chromedriver.exe')
 driver.get('https://vietstock.vn/chung-khoan.htm')
 sleep(3)
 count_page = 0
+count_id = 0
 
 def Crawl_10_article():
     global count_page
@@ -23,20 +24,27 @@ def Crawl_10_article():
         url = link.get_attribute("href")
         #print(url)
         urls.append(url)
-
+        
+    with open('Vietstock.csv', 'w', encoding='utf-8', newline='') as file_output:
+        headers = ['id', 'tittle', 'content']
+        writer = csv.DictWriter(file_output, delimiter=',', lineterminator='\n', fieldnames=headers)
+        writer.writeheader()
+        #10 urls in 1 page
     #10 urls in 1 page
-    for url in urls:
-        count += 1
-        print(count)
-        driver.get(url)
-        sleep()
-        #Tìm và in tiêu đề bài báo
-        tittle = driver.find_element_by_class_name('article-title')
-        ChuanHoa(tittle.text)
-        #Tìm và in nội dung bài báo
-        content = driver.find_element_by_xpath('/html/body/section[1]/section/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[4]')
-        ChuanHoa(content.text)
+        for url in urls:
+            count += 1
+            print(count)
+            driver.get(url)
+            sleep(4)
+            #Tìm và in tiêu đề bài báo
+            tittle = driver.find_element_by_class_name('article-title')
+            ChuanHoa(tittle.text)
+            #Tìm và in nội dung bài báo
+            content = driver.find_element_by_xpath('/html/body/section[1]/section/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[4]')
+            ChuanHoa(content.text)
+            sleep(3)
 
+            writer.writerow({headers[0]:count_id, headers[1]:list_tittle, headers[2]:list_content})
 def NextPage():
     next_page = driver.find_element_by_xpath('//*[@id="page-next "]/a')
     driver.execute_script("arguments[0].click();", next_page)
@@ -53,7 +61,7 @@ While True:
     for i in range(count_page):
         NextPage()
     Crawl_10_article()
-    #if count_page == 3:
-        #break
+    if count_page == 100:
+        break
 
 driver.close()
